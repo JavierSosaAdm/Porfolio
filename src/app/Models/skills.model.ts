e7ee1a917dc7bae8175aa8657206362349de54ea
JavierSosaAdm <javier.sosa.1911@gmail.com>
2026-07-28 22:35:03 -0300

    estilos en los items de nav-bar modificacion en el modelo de skills y href de los iconos a las documentacions

--- src/app/Models/skills.model.ts
+++ src/app/Models/skills.model.ts
@@ -1,4 +1,6 @@
 export interface Skill {
     skillName:            string;
-    image:           string;
+    image:                string;
+    documentation:        string;
+    img:                  string;
 }
